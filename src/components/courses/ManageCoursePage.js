b7b89ac204696396cc77610fe8239c66c1bcc42f
Daniel Deutsch <deudan1010@gmail.com>
2017-07-14 17:26:12 +0200

    add Redux testing

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,8 +5,9 @@ import toastr from 'toastr';
 
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import { authorsFormattedForDropdown } from '../../selectors/selectors';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
 	constructor(props, context) {
 		super(props, context);
 		this.state = {
@@ -31,8 +32,24 @@ class ManageCoursePage extends React.Component {
 		return this.setState({ course: course });
 	}
 
+	courseFormIsValid() {
+		let formIsValid = true;
+		let errors = {};
+
+		if (this.state.course.title.length < 5) {
+			errors.title = 'Title must be at least 5 characters.';
+			formIsValid = false;
+		}
+
+		this.setState({ errors: errors });
+		return formIsValid;
+	}
+
 	saveCourse(event) {
 		event.preventDefault();
+		if (!this.courseFormIsValid()) {
+			return;
+		}
 		this.setState({ saving: true });
 		this.props.actions
 			.saveCourse(this.state.course)
@@ -93,15 +110,9 @@ function mapStateToProps(state, ownProps) {
 		course = getCourseById(state.courses, courseId);
 	}
 
-	const authorsFormattedForDropdown = state.authors.map(author => {
-		return {
-			value: author.id,
-			text: author.firstName + ' ' + author.lastName
-		};
-	});
 	return {
 		course: course,
-		authors: authorsFormattedForDropdown
+		authors: authorsFormattedForDropdown(state.authors)
 	};
 }
 
